0aee8fe59bb5b2bfac987b53dc4557657c414dd3
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2010-10-17 22:39:46 +0530

    Comments added. Readme modified

--- pdf_crop.sh
+++ pdf_crop.sh
@@ -1,1 +1,5 @@
-for i in *.pdf ; do gs -dSAFER -dNOPAUSE -dBATCH -sDEVICE=bbox "$i" 2> bounding ; sh /usr/bin/pdfmanipulate crop -o "${i%.pdf}-cropped.pdf" -b bounding "$i"; done
+# A script to remove white-spaces around text in pdfs in a folder to easily read them in Nook eReader.
+for i in *.pdf ;
+ do gs -dSAFER -dNOPAUSE -dBATCH -sDEVICE=bbox "$i" 2> bounding ;
+ sh /usr/bin/pdfmanipulate crop -o "${i%.pdf}-cropped.pdf" -b bounding "$i";
+ done
--- pdf_rotate.sh
+++ pdf_rotate.sh
@@ -1,3 +1,5 @@
+# A script to rotate presentation pdfs in a folder to easily read them in Nook eReader.
+
 for i in *.pdf ;
 do pdftk "$i" cat 1-endE output "${i%.pdf}-rotated.pdf";
 done
